f25bd96641ae02b4d723ef0f626d8c35508968d4
Vishal Patil <vishpat@gmail.com>
2012-10-03 10:05:36 -0400

    Removed the clearing of stencil buffer from init

--- stl_viewer.c
+++ stl_viewer.c
@@ -338,7 +338,7 @@ init(char *filename)
 	glShadeModel(GL_SMOOTH);
 
         glClearColor(135.0 / 255, 206.0 / 255.0, 250.0 / 255.0, 0.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glColor3f(120.0 / 255.0 , 120.0 / 255.0, 120.0 / 255.0);
 
         glEnable(GL_NORMALIZE);
